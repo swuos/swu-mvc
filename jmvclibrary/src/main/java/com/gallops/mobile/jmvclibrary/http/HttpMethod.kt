8b831616a81c0ac325323d07ebd957e6dbc94afc
wangyu <1837960253@qq.com>
2018-05-28 19:16:26 +0800

    更新包名

--- jmvclibrary/src/main/java/com/gallops/mobile/jmvclibrary/http/HttpMethod.kt
+++
@@ -1,9 +0,0 @@
-package com.gallops.mobile.jmvclibrary.http
-
-/**
- * http请求方式
- * Created by wangyu on 2018/1/20.
- */
-enum class HttpMethod(val method: String) {
-    GET("GET"), POST("POST")
-}
